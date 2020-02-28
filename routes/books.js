aea66ac4b2d73213c09a64f139aede1d8172c758
Yimeng Yu <yimeng.yu@gmail.com>
2020-02-28 12:49:15 -0800

    update axios

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,11 @@
+const axios = require("axios");
 const router = require("express").Router();
-const booksController = require("../controllers/booksController");
 
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  // .put(booksController.update)
-  .delete(booksController.remove);
+router.get("/books", (req, res) => {
+  axios
+    .get("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAjRJsbePuVu0Qax_C6kfO0C1soknYdkPo", { params: req.query })
+    .then(results => res.json(results.data))
+    .catch(err => res.status(422).json(err));
+});
 
 module.exports = router;
