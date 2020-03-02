47aba8d6c9a6c8ca92246a6e7914fccf772f34e2
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-02 00:00:42 -0800

    update search.js with display books

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
