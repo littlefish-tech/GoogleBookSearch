cf4a4ea9b5924ad2defa522bd34d69e3d8a8c44e
Yimeng Yu <yimeng.yu@gmail.com>
2020-04-06 22:45:05 -0700

    update footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Saved from "./pages/Saved";
 // import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/saved" component={Saved} />
           {/* <Route component={NoMatch} /> */}
         </Switch>
+        <Footer><a href="https://vast-garden-33544.herokuapp.com/" style={{color: "black"}}>@ 2020 Google Book Search Copyright</a></Footer>
       </div>
     </Router>
   );
